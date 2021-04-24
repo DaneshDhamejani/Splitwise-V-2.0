d94197a14933e0b7cac5828331419baa17d59456
Danesh2497 <danesh@aigen.tech>
2021-04-23 21:09:03 -0700

    FInal Changes

--- Frontend/src/reducers/index.js
+++ Frontend/src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import signupreducer from  "./signupreducer";
 import loginreducer from "./loginreducer";
 
 
 const rootReducer = combineReducers({
-    
+    form: formReducer,
     login : loginreducer,
     usersignup : signupreducer,
   });
